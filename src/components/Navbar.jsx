9590cf877b1f8bd8b057551037684747034255d7
riaz113 <riazmahamud113@gmail.com>
2026-06-14 19:15:35 +0600

    start assignment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import image1 from "@/assets/icons8-cow-48 (1).png";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  const NavLink = (
+    <>
+      <li>
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link href={""}>All Animals</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-[#14532D] text-white">
+      <div className="navbar max-w-full md:max-w-11/12 mx-auto">
+        <div className="navbar-start ">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {NavLink}
+            </ul>
+          </div>
+          <a className="text-xl flex justify-center items-center gap-2 font-bold">
+            <Image className="bg-yellow-500 p-1 rounded-2xl" src={image1} width={40} height={40} alt="logo"></Image>
+            QurbaniHat
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{NavLink}</ul>
+        </div>
+        <div className="navbar-end gap-4">
+          <a className="btn">LogIn</a>
+          <a className="btn">Register</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
